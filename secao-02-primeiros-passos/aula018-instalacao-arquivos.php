5cda0d1fe76cbe7ae335c0102c5d0ef702f3fc03
ErickFSilva <erick_ferreira@outlook.com>
2024-01-02 18:49:46 -0300

    secao-03-criando-layouts, 02/01/2024 (tarde)

---
+++ secao-02-primeiros-passos/aula018-instalacao-arquivos.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap 5.3</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="../bootstrap-5.3/css/bootstrap.css">
+    <script src="../bootstrap-5.3/js/bootstrap.js" defer></script>
+</head>
+<body>
+
+    <h1>Testando Bootstrap por arquivos</h1>
+    
+</body>
+</html>
